2ebf12f31cd9dae0bd60c8a84c1b035bea082ee5
Cornel Cruceru <amne@amne.me>
2024-09-17 11:37:55 +0300

    replace tmux-sessionizer with prime's shell script; starship bling bling

--- dots/init/scripts/70-install-termtools.sh
+++ dots/init/scripts/70-install-termtools.sh
@@ -1,9 +1,12 @@
 # starship
 brew install starship nvim
+cp ../../etc/starship.toml ~/.config/starship.toml
 
 # tms
-. ~/.cargo/env
-cargo install tmux-sessionizer
+# . ~/.cargo/env
+# cargo install tmux-sessionizer
+mkdir -p ~/bin
+cp ../../bin/tmx ~/bin/tmx
 
 mkdir ~/work
 tms config --full-path true -p ~/work
